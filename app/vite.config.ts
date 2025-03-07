4d04ef6bee1da1b7ad2c97c085bc1dda75aadc4f
Mark Oh <oms3749@gmail.com>
2025-03-07 23:51:38 +0900

    add: relay runtime

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import relay from "vite-plugin-relay-lite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), relay()],
 });
